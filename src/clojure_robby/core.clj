da202748c4ad7c22e69a7a00008597ba2db4e150
osense <krupicka.adam@gmail.com>
2015-05-21 11:46:05 +0200

    Improved (rand-map) and docs.

--- src/clojure_robby/core.clj
+++ src/clojure_robby/core.clj
@@ -9,11 +9,11 @@
 (def map-size 20) ; Size of the map used for robot simulation.
 (def gold-prob 10) ; Probability in 100 that a tile contains gold.
 (def simul-steps 100) ; Steps to allow the robot to make in a simulation.
-(def tiles [\w \p \g]) ; Tiles that populate the map.
-(def actions [\u \d \l \r \p]) ; Actions the robot can take.
+(def tiles [\w \p \g]) ; Tiles that populate the map: wall, path, and gold.
+(def actions [\u \d \l \r \p]) ; Actions the robot can take: up, down, left, right, pick up.
 
 
-; DNA functios
+; DNA functions
 (defn rand-dna []
   "Initializes a vector of random DNA."
   (defn rand-gene []
@@ -22,23 +22,23 @@
 
 (defn cross [a b]
   "Crosses two vectors of DNA."
-  (defn choose-rand [a b]
-    (if (< (rand-int 100) 50) a b))
+  (defn choose-rand [x y]
+    (if (< (rand-int 100) 50) x y))
   (map choose-rand a b))
 
 
 ; Fitness evaluation functions
 (defn rand-map []
   "Initializes a map with walls at the boundaries and randomly positioned gold."
-  (def insides (- map-size 2))
-  (def wall-row (vec (repeat map-size \w)))
-  (defn rand-tile []
-    (if (< (rand-int 100) gold-prob) \g \p))
-  (defn rand-row []
-    (vec (concat [\w] (repeatedly insides rand-tile) [\w])))
-  (vec (concat [wall-row] (repeatedly insides rand-row) [wall-row])))
+  (defn gen-tile [x y]
+    (def last-coord(- map-size 1))
+    (if (or (= x 0) (= y 0) (= x last-coord) (= y last-coord))
+      \w
+      (if (< (rand-int 100) gold-prob) \g \p)))
+  (map (fn [y] (map (fn [x] (gen-tile x y)) (range map-size))) (range map-size)))
 
 (defn on-index [themap x y]
+  "Given a map and coordinates, evaluates to what is on the map at the position."
   (nth (nth themap y) x))
 
 ;(defn simulate [dna onmap]
