0d0a7ebba48916be43266e13a69214d50d0e73be
osense <krupicka.adam@gmail.com>
2015-05-25 00:07:05 +0200

    Imporved the python plotter script.

--- util/plotter.py
+++ util/plotter.py
@@ -4,16 +4,30 @@ import sys
 import os
 from matplotlib import pyplot as plt
 
-infile = sys.argv[1]
-vals = []
-with open(infile, 'r') as f:
-    for line in f:
-        vals.append(float(line))
+def read_file(fname):
+    v = []
+    with open(fname, 'r') as f:
+        for line in f:
+            v.append(float(line))
+    return v
 
-plt.plot([i for i in range(len(vals))], vals)
+legend_title = None
+for arg in sys.argv[1:]:
+    try:
+        vals = read_file(arg)
+        plt.plot(vals, label = arg)
+    except:
+        if arg[0] != '-':
+            legend_title = arg
+        print('skipping ' + arg)
+        pass
+
 plt.xlabel('Generation')
 plt.ylabel('Fitness')
+plt.legend(loc = 4, title = legend_title)
 
-(out, _) = os.path.splitext(infile)
-plt.savefig(out + '.png', bbox_inches='tight')
+if '-s' in sys.argv:
+    plt.show()
+else:
+    plt.savefig('out-graph.png', bbox_inches='tight')
 
